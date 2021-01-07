7aa4c10ecf0425f855b6e6bc66fb3fc27fe8f5da
Dmitry "Ventan" Teplov <Ventantogame@gmail.com>
2021-01-07 02:49:09 +0100

    connecting with server fixed

--- src/main/java/me/ventan/venAuth/events/Login.java
+++ src/main/java/me/ventan/venAuth/events/Login.java
@@ -4,13 +4,14 @@ package me.ventan.venAuth.events;
 import me.ventan.venAuth.Main;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -21,52 +22,53 @@ import java.nio.ByteBuffer;
 import java.util.logging.Level;
 
 public class Login implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onLogin(PlayerJoinEvent event){
-        if(Main.getProtectedNicks().contains(event.getPlayer().getDisplayName())){
-                Thread runnable = new Thread() {
-                    @Override
-                    public void run() {
+        if(Main.getProtectedNicks().contains(event.getPlayer().getName())){
+            System.out.println("first call");
+            Main.getInstance().getServer().getScheduler().runTask(Main.getInstance(), new BukkitRunnable() {
+                @Override
+                public void run() {
+                    BukkitRunnable task = this;
+                    BukkitTask task1 = Main.getInstance().getServer().getScheduler().runTask(Main.getInstance(),()->{
                         try {
-                            String ip = "146.59.3.145";
-                            int port = 7584;
-                            InetSocketAddress address = new InetSocketAddress(ip, port);
-                            Socket socket = new Socket();
-                            socket.connect(address);
-                            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-                            DataInputStream dis = new DataInputStream(socket.getInputStream());
-                            JSONObject object = new JSONObject();
-                            object.put("function", "checkUser");
-                            JSONObject data = new JSONObject();
-                            data.put("ip", ByteBuffer.wrap(event.getPlayer().getAddress().getAddress().getAddress()).getInt());
-                            data.put("nick", event.getPlayer().getDisplayName());
-                            object.put("data", data);
-                            dos.writeUTF(object.toString());
-                            String response = dis.readUTF();
-                            socket.close();
-                            JSONTokener tokener = new JSONTokener(response);
-                            JSONObject myresponse = new JSONObject(tokener);
-                            if (!myresponse.getBoolean("success")) {
-                                event.getPlayer().kickPlayer("Potwierdzenie wysłane na aplikację! Masz 15 minut na potwierdzenie osobowości");
-                            } else {
-                                event.getPlayer().sendMessage("Weryfikacja venAuth aktywna!");
-                            }
-                        } catch (IOException e) {
-                            Main.getInstance().getLogger().log(Level.SEVERE,"Server connection exception in VenPlug");
+                            Thread.sleep(10000);
+                            Main.getInstance().getLogger().log(Level.SEVERE,"Serwer autoryzacji jest wyłączony!");
+                            event.getPlayer().kickPlayer(ChatColor.RED +"Serwer autoryzacji jest wyłączony! Napisz do supportu na servernetpl@gmail.com");
+                            Main.getInstance().getServer().getScheduler().cancelTask(task.getTaskId());
+                            System.out.println("canceling");
+                        } catch (InterruptedException e) {
                         }
-                    }
-                };
-                runnable.run();
-                Thread timeoutthread = new Thread(()->{
+                    });
                     try {
-                        Thread.sleep(5000);
-                        Main.getInstance().getLogger().log(Level.SEVERE,"Serwer autoryzacji jest wyłączony!");
-                        event.getPlayer().kickPlayer(ChatColor.RED +"Serwer autoryzacji jest wyłączony! Napisz do supportu na servernetpl@gmail.com");
-                        runnable.interrupt();
-                    } catch (InterruptedException e) {
+                        String ip = "146.59.3.145";
+                        int port = 7584;
+                        InetSocketAddress address = new InetSocketAddress(ip, port);
+                        Socket socket = new Socket();
+                        socket.connect(address);
+                        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                        DataInputStream dis = new DataInputStream(socket.getInputStream());
+                        JSONObject object = new JSONObject();
+                        object.put("function", "checkUser");
+                        object.put("ip", String.valueOf(ByteBuffer.wrap(event.getPlayer().getAddress().getAddress().getAddress()).getInt()));
+                        object.put("nick", event.getPlayer().getName());
+                        dos.writeUTF(object.toString());
+                        String response = dis.readUTF();
+                        task1.cancel();
+                        System.out.println("got response");
+                        socket.close();
+                        JSONTokener tokener = new JSONTokener(response);
+                        JSONObject myresponse = new JSONObject(tokener);
+                        if (!myresponse.getBoolean("success")) {
+                            event.getPlayer().kickPlayer("Potwierdzenie wysłane na aplikację! Masz 15 minut na potwierdzenie osobowości");
+                        } else {
+                            event.getPlayer().sendMessage("Weryfikacja venAuth aktywna!");
+                        }
+                    } catch (IOException e) {
+                        Main.getInstance().getLogger().log(Level.SEVERE,"Server connection exception in VenPlug");
                     }
-                });
-                timeoutthread.start();
+                }
+            });
         }
     }
 }
