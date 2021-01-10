89ad9af2f55bb8e0f0bb0f80e58eb0e33cd1e4f7
Ventan00 <Ventantogame@gmail.com>
2021-01-10 03:01:20 +0100

    fix for sth

--- src/main/java/me/ventan/venAuth/events/Login.java
+++ src/main/java/me/ventan/venAuth/events/Login.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPreLoginEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.json.JSONObject;
@@ -15,6 +16,7 @@ import org.json.JSONTokener;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -25,21 +27,29 @@ public class Login implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onLogin(PlayerJoinEvent event){
         if(Main.getProtectedNicks().contains(event.getPlayer().getName())){
-            System.out.println("first call");
-            Main.getInstance().getServer().getScheduler().runTask(Main.getInstance(), new BukkitRunnable() {
+            Main.getInstance().getLogger().info(ChatColor.RED+" OP player connected, sending verification request");
+            Thread thread = new Thread() {
                 @Override
-                public void run() {
-                    BukkitRunnable task = this;
-                    BukkitTask task1 = Main.getInstance().getServer().getScheduler().runTask(Main.getInstance(),()->{
-                        try {
-                            Thread.sleep(10000);
-                            Main.getInstance().getLogger().log(Level.SEVERE,"Serwer autoryzacji jest wyłączony!");
-                            event.getPlayer().kickPlayer(ChatColor.RED +"Serwer autoryzacji jest wyłączony! Napisz do supportu na servernetpl@gmail.com");
-                            Main.getInstance().getServer().getScheduler().cancelTask(task.getTaskId());
-                            System.out.println("canceling");
-                        } catch (InterruptedException e) {
+                public void run(){
+                    Thread me = this;
+                    Thread authTimeout = new Thread(){
+                        @Override
+                        public void run(){
+                            try {
+                                Thread.sleep(10000);
+                                Main.getInstance().getLogger().log(Level.SEVERE,"Serwer autoryzacji jest wyłączony!");
+                                Main.getInstance().getServer().getScheduler().runTask(Main.getInstance(), new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        event.getPlayer().kickPlayer(ChatColor.RED +"Serwer autoryzacji jest wyłączony! Napisz do supportu na servernetpl@gmail.com");
+                                    }
+                                });
+                                me.interrupt();
+                            } catch (InterruptedException e) {
+                            }
                         }
-                    });
+                    };
+                    authTimeout.start();
                     try {
                         String ip = "77.55.209.66";
                         int port = 7584;
@@ -54,21 +64,34 @@ public class Login implements Listener {
                         object.put("nick", event.getPlayer().getName());
                         dos.writeUTF(object.toString());
                         String response = dis.readUTF();
-                        task1.cancel();
-                        System.out.println("got response");
+                        authTimeout.interrupt();
+                        Main.getInstance().getLogger().info("Got response from serwer: "+response);
                         socket.close();
                         JSONTokener tokener = new JSONTokener(response);
                         JSONObject myresponse = new JSONObject(tokener);
                         if (!myresponse.getBoolean("success")) {
-                            event.getPlayer().kickPlayer("Potwierdzenie wysłane na aplikację! Masz 15 minut na potwierdzenie osobowości");
+                            Main.getInstance().getServer().getScheduler().runTask(Main.getInstance(), new Runnable() {
+                                @Override
+                                public void run() {
+                                    event.getPlayer().kickPlayer("Potwierdzenie wysłane na aplikację! Masz 15 minut na potwierdzenie osobowości");
+                                }
+                            });
                         } else {
-                            event.getPlayer().sendMessage("Weryfikacja venAuth aktywna!");
+                            Main.getInstance().getServer().getScheduler().runTask(Main.getInstance(), new Runnable() {
+                                @Override
+                                public void run() {
+                                    event.getPlayer().sendMessage(ChatColor.AQUA+"Weryfikacja venAuth aktywna!");
+                                }
+                            });
                         }
                     } catch (IOException e) {
                         Main.getInstance().getLogger().log(Level.SEVERE,"Server connection exception in VenPlug");
                     }
+
+
                 }
-            });
+            };
+            thread.start();
         }
     }
 }
